cf1d00a6480a6bcf16b03dd058cef51fd0068d8c
algosal <pfc.salman@gmail.com>
2022-11-28 21:06:20 -0500

    Golden Principles Initiated

--- src/components/fix/golden.jsx
+++ src/components/fix/golden.jsx
@@ -11,14 +11,15 @@ export default function GoldenPrinciples() {
 
         <p><h1 style={{color:"maroon"}}>Golden Principles</h1></p>
         <p className="big-font-p" style={{width:"80%", paddingLeft:"100px"}}>
-          It is the knowledge of Perfect Law working through and above all things; 
-            of perfect justice operating in and adjusting all human affairs, 
-            that enables the good man to love his enemies, 
-            and to rise above all hatred, resentment or complaining; 
-            for he knows that only his own can come to him, and that 
-            though he be surrounded by persecutors, his enemies are but the 
-            blind instruments of a faultless retribution; and so he blamesthem not, 
-            and calmly receives his accounts and patiently pays his moral debt. </p>
+          The Principle Below must be read everyday and Followed 
+          <ul>
+            <li>Definiteness of Purpose</li>
+            <li>Mastery Over Self</li>
+            <li>Learning from Adversity</li>
+            <li>Environmental Influence</li>
+            <li>Time</li>
+          </ul>
+           </p>
     <button onClick={myFunc} className="button-entry">&emsp; Open Main Page &emsp;</button>
     </div>
   );
